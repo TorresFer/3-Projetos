7fc425fa7ab190a0479f3ee32bbab6db24fee576
Fernando <93451006+TorresFer@users.noreply.github.com>
2022-04-01 15:08:32 -0300

    Delete Web_Curso directory

--- Web_Curso/Conceitos Web/HtmlCssJs.js
+++
@@ -1,8 +0,0 @@
-/*
-HTML - Estrutural: Título, Menu, Conteúdo
-    - Ele faz a referencias dos arquivos CSS e JS; dependendo do framework, ex React, ele junta
-    as tecnologias para separar Componentes de negócios.
-CSS - Estilos: Design, lados, rodapé etc.
-JS - Comportamento dinâmico: menus, listagens etc.
-
-*/
